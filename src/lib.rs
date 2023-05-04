0cff8573c91bae5abcd77d31550f65ae4f666de2
tjensen <tim.jensen@vector.com>
2023-05-04 17:36:48 +0200

    comparison of closure with and without Result return types

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,58 @@ pub struct Color {
     pub b: u8,
 }
 
-// Deserialize some struct with 3 Fields
+#[derive(Debug)]
+pub enum Error {
+    NotEnoughBytes,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+pub fn deserialize_u8(reader: &mut &[u8], v: &mut u8) -> Result<()> {
+    if reader.remaining() < core::mem::size_of::<u8>() {
+        return Err(Error::NotEnoughBytes);
+    }
+    *v = reader.get_u8();
+    Ok(())
+}
+
+///
+/// RESULT
+///
+
+pub fn deserialize_struct_lib_result<S, F>(
+    reader: &mut &[u8],
+    data: &mut S,
+    func: &[F; 3],
+) -> Result<()>
+where
+    F: Fn(&mut &[u8], &mut S) -> Result<()>,
+{
+    if reader.remaining() < 3 {
+        return Err(Error::NotEnoughBytes);
+    }
+    func[0](reader, data)?;
+    func[1](reader, data)?;
+    func[2](reader, data)?;
+    Ok(())
+}
+
 #[inline(never)]
-pub fn deserialize_struct<S, F>(reader: &mut &[u8], data: &mut S, func: &[F; 3])
+pub fn deserialize_color_generic_lib_result(reader: &mut &[u8], color: &mut Color) -> Result<()> {
+    deserialize_struct_lib_result(reader, color, &DESER_COLOR_CLOSURE_LIB_RESULT)
+}
+
+const DESER_COLOR_CLOSURE_LIB_RESULT: [fn(&mut &[u8], &mut Color) -> Result<()>; 3] = [
+    |r, s| deserialize_u8(r, &mut s.r),
+    |r, s| deserialize_u8(r, &mut s.g),
+    |r, s| deserialize_u8(r, &mut s.b),
+];
+
+///
+/// PLAIN
+///
+
+pub fn deserialize_struct_lib_plain<S, F>(reader: &mut &[u8], data: &mut S, func: &[F; 3])
 where
     F: Fn(&mut &[u8], &mut S),
 {
@@ -20,13 +69,12 @@ where
     }
 }
 
-// Implement the generic function in the library
-pub fn deserialize_color_generic(reader: &mut &[u8], color: &mut Color) {
-    deserialize_struct(reader, color, &DESER_COLOR_CLOSURE_LIB)
+#[inline(never)]
+pub fn deserialize_color_generic_lib_plain(reader: &mut &[u8], color: &mut Color) {
+    deserialize_struct_lib_plain(reader, color, &DESER_COLOR_CLOSURE_LIB_PLAIN);
 }
 
-// Closure array to deserialize a Color struct
-pub const DESER_COLOR_CLOSURE_LIB: [fn(&mut &[u8], &mut Color); 3] = [
+const DESER_COLOR_CLOSURE_LIB_PLAIN: [fn(&mut &[u8], &mut Color); 3] = [
     |r, s| s.r = r.get_u8(),
     |r, s| s.g = r.get_u8(),
     |r, s| s.b = r.get_u8(),
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,66 @@
 use std::hint::black_box;
 
 use bytes::Buf;
-use rust_lib_closure_test::{deserialize_color_generic, deserialize_struct, Color};
+use rust_lib_closure_test::{
+    deserialize_color_generic_lib_plain, deserialize_color_generic_lib_result,
+    deserialize_struct_lib_plain, Result,
+};
+use rust_lib_closure_test::{deserialize_struct_lib_result, deserialize_u8, Color};
 
 pub fn main() {
     let buf: Vec<u8> = Vec::from([0x01, 0x02, 0x03]);
 
-    // Call the generic function indirect (implemented in lib)
+    // LIB RESULT
     let mut color = Color::default();
     let cursor = &mut buf.as_slice();
-    deserialize_color_generic(black_box(cursor), &mut color);
+    deserialize_color_generic_lib_result(black_box(cursor), &mut color).unwrap();
     println!("color: {:?}", color);
 
-    // Call the generic function direct
+    // MAIN RESULT
     let mut color = Color::default();
     let cursor = &mut buf.as_slice();
-    deserialize_struct(black_box(cursor), &mut color, &DESER_COLOR_CLOSURE_BIN);
+    deserialize_color_generic_main_result(black_box(cursor), &mut color).unwrap();
+    println!("color: {:?}", color);
+
+    // LIB PLAIN
+    let mut color = Color::default();
+    let cursor = &mut buf.as_slice();
+    deserialize_color_generic_lib_plain(black_box(cursor), &mut color);
+    println!("color: {:?}", color);
+
+    // MAIN PLAIN
+    let mut color = Color::default();
+    let cursor = &mut buf.as_slice();
+    deserialize_color_generic_main_plain(black_box(cursor), &mut color);
     println!("color: {:?}", color);
 }
 
-const DESER_COLOR_CLOSURE_BIN: [fn(&mut &[u8], &mut Color); 3] = [
+///
+/// RESULT
+///
+
+const DESER_COLOR_CLOSURE_MAIN_RESULT: [fn(&mut &[u8], &mut Color) -> Result<()>; 3] = [
+    |r, s| deserialize_u8(r, &mut s.r),
+    |r, s| deserialize_u8(r, &mut s.g),
+    |r, s| deserialize_u8(r, &mut s.b),
+];
+
+#[inline(never)]
+fn deserialize_color_generic_main_result(reader: &mut &[u8], color: &mut Color) -> Result<()> {
+    deserialize_struct_lib_result(reader, color, &DESER_COLOR_CLOSURE_MAIN_RESULT)
+}
+
+///
+/// PLAIN
+///
+
+const DESER_COLOR_CLOSURE_MAIN_PLAIN: [fn(&mut &[u8], &mut Color); 3] = [
     |r, s| s.r = r.get_u8(),
     |r, s| s.g = r.get_u8(),
     |r, s| s.b = r.get_u8(),
 ];
+
+#[inline(never)]
+fn deserialize_color_generic_main_plain(reader: &mut &[u8], color: &mut Color) {
+    deserialize_struct_lib_plain(reader, color, &DESER_COLOR_CLOSURE_MAIN_PLAIN);
+}
